3efdadd51603d931371d1593261b7bcbab9e6e55
Matthew Davey <matthew.davey@dotbunny.com>
2021-09-20 15:35:42 -0400

    Add DeleteFolder / WriteFile options

---
+++ K9.Setup/Verbs/DeleteFolder.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+
+namespace K9.Setup.Verbs
+{
+    [Verb("DeleteFolder")]
+    public class DeleteFolder : IVerb
+    {
+        [Option('f', "folder", Required = false, HelpText = "Path to folder to remove.")]
+        public string Folder { get; set; }
+
+        public bool CanExecute()
+        {
+            return true;
+        }
+
+        public bool Execute()
+        {
+            if (System.IO.Directory.Exists(Folder))
+            {
+                System.IO.Directory.Delete(Folder, true);
+            }
+            return true;
+        }
+    }
+}
---
+++ K9.Setup/Verbs/WriteFile.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+
+namespace K9.Setup.Verbs
+{
+    [Verb("WriteFile")]
+    public class WriteFile : IVerb
+    {
+        [Option('f', "file", Required = false, HelpText = "Path to file to write too.")]
+        public string File { get; set; }
+        
+        [Option('c', "content", Required = false, HelpText = "The content of the file to be written")]
+        public string Content { get; set; }
+
+        public bool CanExecute()
+        {
+            return !string.IsNullOrEmpty(File);
+        }
+
+        public bool Execute()
+        {
+            var folder = System.IO.Path.GetDirectoryName(File);
+            if (!System.IO.Directory.Exists(folder))
+            {
+                System.IO.Directory.CreateDirectory(folder);
+            }
+            
+            System.IO.File.WriteAllText(File, Content);
+            return true;
+        }
+    }
+}
