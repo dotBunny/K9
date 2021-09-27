e621532ff261cfd58cc0c92d22d1a325554f0401
Matthew Davey <matthew.davey@dotbunny.com>
2021-09-27 15:19:00 -0400

    Add initial stub for Sendsafely app

---
+++ K9.SendSafely/Verbs/Download.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2018-2021 dotBunny Inc.
+
+namespace K9.Setup.Verbs
+{
+    public class Download
+    {
+        public bool CanExecute()
+        {
+            return true;
+        }
+
+        public bool Execute()
+        {
+            return true;
+        }
+    }
+}
