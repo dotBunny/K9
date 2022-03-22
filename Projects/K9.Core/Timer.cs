bf6383b5e0e779394470612ce4b9e70f168347c0
Matthew Davey <matthew.davey@dotbunny.com>
2022-03-22 12:20:05 -0400

    Raw replacement to solve for issue pushing

--- Projects/K9.Core/Timer.cs
+++ Projects/K9.Core/Timer.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2018-2021 dotBunny Inc.
+// Copyright (c) 2018-2021 dotBunny Inc.
 // dotBunny licenses this file to you under the BSL-1.0 license.
 // See the LICENSE file in the project root for more information.
 
@@ -37,7 +37,13 @@ namespace K9
 
         public string TransferRate(long byteCount)
         {
-            float speed = (byteCount / 125000) / (_stopwatch.ElapsedMilliseconds / 1000);
+            float divisor = _stopwatch.ElapsedMilliseconds / 1000f;
+            if (divisor == 0)
+            {
+                return string.Empty;
+            }
+
+            float speed = (byteCount / 125000f) / divisor;
             return $"{System.Math.Round(speed, 2)} Mbps";
         }
     }
