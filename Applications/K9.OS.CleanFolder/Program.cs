eb3c126cc8e7ebec27a86595a8faa785c5f51abf
Matthew Davey <reapazor@gmail.com>
2026-03-09 21:41:16 -0400

    Add stubs to other quick to build out

--- Applications/K9.OS.CleanFolder/Program.cs
+++ Applications/K9.OS.CleanFolder/Program.cs
@@ -1,11 +1,32 @@
 ﻿// Copyright dotBunny Inc. All Rights Reserved.
 // See the LICENSE file at the repository root for more information.
 
+using System;
+using K9.Core;
+
 namespace K9.OS.CleanFolder;
 
 internal static class Program
 {
     static void Main()
     {
+        using ConsoleApplication framework = new(
+            new ConsoleApplicationSettings()
+            {
+                // ReSharper disable once StringLiteralTypo
+                DefaultLogCategory = "CLEANFOLDER",
+                LogOutputs = [new Core.LogOutputs.ConsoleLogOutput()]
+            }, new CleanFolderProvider());
+
+        try
+        {
+            CleanFolderProvider provider = (CleanFolderProvider)framework.ProgramProvider;
+
+
+        }
+        catch (Exception ex)
+        {
+            framework.ExceptionHandler(ex);
+        }
     }
 }
--- Applications/K9.OS.SetEnvironmentVariable/Program.cs
+++ Applications/K9.OS.SetEnvironmentVariable/Program.cs
@@ -1,11 +1,32 @@
 ﻿// Copyright dotBunny Inc. All Rights Reserved.
 // See the LICENSE file at the repository root for more information.
 
+using System;
+using K9.Core;
+
 namespace K9.OS.SetEnvironmentVariable;
 
 internal static class Program
 {
     static void Main()
     {
+        using ConsoleApplication framework = new(
+            new ConsoleApplicationSettings()
+            {
+                // ReSharper disable once StringLiteralTypo
+                DefaultLogCategory = "SETENV",
+                LogOutputs = [new Core.LogOutputs.ConsoleLogOutput()]
+            }, new SetEnvironmentVariableProvider());
+
+        try
+        {
+            SetEnvironmentVariableProvider provider = (SetEnvironmentVariableProvider)framework.ProgramProvider;
+
+
+        }
+        catch (Exception ex)
+        {
+            framework.ExceptionHandler(ex);
+        }
     }
 }
