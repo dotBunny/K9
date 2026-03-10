584e7e33d3543d46107639f226ccc00ca1d6d570
Matthew Davey <reapazor@gmail.com>
2026-03-09 20:37:59 -0400

    Minor tweaks to help options

--- Applications/K9.Workspace.Reset/ResetProvider.cs
+++ Applications/K9.Workspace.Reset/ResetProvider.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright dotBunny Inc. All Rights Reserved.
 // See the LICENSE file at the repository root for more information.
 
+using System.Collections.Generic;
 using K9.Core;
 using K9.Core.Modules;
 using K9.Core.Utils;
@@ -14,6 +15,16 @@ public class ResetProvider : ProgramProvider
     public override string GetDescription()
     {
         return "A tool for resetting the state of specific areas of the project.";
+    }
+
+    public override KeyValuePair<string, string>[] GetFlagHelp()
+    {
+        KeyValuePair<string, string>[] lines = new KeyValuePair<string, string>[2];
+
+        lines[0] = new KeyValuePair<string, string>("PROJECT", "Delete project `Intermediate` and `Binaries` folders.");
+        lines[1] = new KeyValuePair<string, string>("PROJECT-PLUGINS", "Delete the `Intermediate` and `Binaries` folders of all plugins in projects.");
+
+        return lines;
     }
 
     public override bool IsValid(ArgumentsModule args)
