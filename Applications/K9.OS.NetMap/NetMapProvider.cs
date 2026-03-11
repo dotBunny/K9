a5a560e209cc1a4161637c7e4ea12b71c69a2d8b
Matthew Davey <reapazor@gmail.com>
2026-03-10 21:31:11 -0400

    Stub out for the provider for net map (will remove function from steamupload

--- Applications/K9.OS.NetMap/NetMapProvider.cs
+++ Applications/K9.OS.NetMap/NetMapProvider.cs
@@ -1,14 +1,88 @@
 ﻿// Copyright dotBunny Inc. All Rights Reserved.
 // See the LICENSE file at the repository root for more information.
 
+using System.Collections.Generic;
 using K9.Core;
+using K9.Core.Extensions;
+using K9.Core.Modules;
 
 namespace K9.OS.NetMap;
 
 public class NetMapProvider : ProgramProvider
 {
+    string? NetworkUsername;
+    string? NetworkPassword;
+    string NetworkDrive = "H"; // We'll add
+    string NetworkShare = @"\\192.168.20.21\Horde"; // This is the farms NAS path to the Horde share
+
     public override string GetDescription()
     {
         return "Provide a mechanism for ensuring network shares are mapped as expected.";
+    }
+
+    public override KeyValuePair<string, string>[] GetArgumentHelp()
+    {
+        KeyValuePair<string, string>[] lines = new KeyValuePair<string, string>[4];
+
+        lines[0] = new KeyValuePair<string, string>("NETWORK-USERNAME",
+            "Username for network access to be established.");
+        lines[1] = new KeyValuePair<string, string>("NETWORK-PASSWORD",
+            "Password for network access to be established.");
+
+        lines[2] = new KeyValuePair<string, string>("NETWORK-DRIVE",
+            "Drive letter to map the network share to. (Optional: H)");
+        lines[3] = new KeyValuePair<string, string>("NETWORK-SHARE",
+            @"Network share path to the Horde share. (Optional: \\192.168.20.21\Horde)");
+
+        return lines;
+    }
+
+    public override bool IsValid(ArgumentsModule args)
+    {
+        if (!args.HasOverrideArgument("NETWORK-USERNAME"))
+        {
+            Log.WriteLine("A NETWORK-USERNAME is required (---NETWORK-USERNAME=myusername)");
+            return false;
+        }
+        if (!args.HasOverrideArgument("NETWORK-PASSWORD"))
+        {
+            Log.WriteLine("A NETWORK-PASSWORD is required (---NETWORK-PASSWORD=mypassword)");
+            return false;
+        }
+
+        if (args.HasOverrideArgument("NETWORK-DRIVE"))
+        {
+            string testDriveLetter = args.GetOverrideArgument("NETWORK-DRIVE");
+            if (testDriveLetter.Length != 1)
+            {
+                Log.WriteLine("NETWORK-DRIVE must be a single letter (---NETWORK-DRIVE=H)");
+                return false;
+            }
+
+            if (testDriveLetter.IsNumeric())
+            {
+                Log.WriteLine("NETWORK-DRIVE cannot be a number (---NETWORK-DRIVE=H)");
+                return false;
+            }
+        }
+
+        return base.IsValid(args);
+    }
+
+    public override void ParseArguments(ArgumentsModule args)
+    {
+        NetworkUsername = args.GetOverrideArgument("NETWORK-USERNAME");
+        NetworkPassword = args.GetOverrideArgument("NETWORK-PASSWORD");
+        if (args.HasOverrideArgument("NETWORK-DRIVE"))
+        {
+            NetworkDrive = args.GetOverrideArgument("NETWORK-DRIVE").ToUpper() + ":";
+        }
+
+        if (args.HasOverrideArgument("NETWORK-SHARE"))
+        {
+            NetworkShare = args.GetOverrideArgument("NETWORK-SHARE");
+        }
+
+        base.ParseArguments(args);
     }
 }
