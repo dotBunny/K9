85826ed03db5e453216425d6ec7cd04f20041521
Matthew Davey <reapazor@gmail.com>
2026-04-30 10:22:03 -0400

    Add OutputToFile

---
+++ Applications/K9.OS.OutputToFile/OutputToFileProvider.cs
@@ -0,0 +1,66 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+using System.Collections.Generic;
+using K9.Core;
+using K9.Core.Modules;
+
+namespace K9.OS.OutputToFile;
+
+public class OutputToFileProvider : ProgramProvider
+{
+    public string? WorkingDirectory = null;
+    public string Command = string.Empty;
+    public string? Arguments;
+    public string Target = string.Empty;
+
+    public override string GetDescription()
+    {
+        return "Wrap execution of applications to control error handling and logging.";
+    }
+
+    public override KeyValuePair<string, string>[] GetArgumentHelp()
+    {
+        KeyValuePair<string, string>[] lines = new KeyValuePair<string, string>[4];
+
+        lines[0] = new KeyValuePair<string, string>("WORKING-DIRECTORY", "Where the command should be executed. (Optional)");
+        lines[1] = new KeyValuePair<string, string>("COMMAND", "The command to execute.");
+        lines[2] = new KeyValuePair<string, string>("ARGUMENTS", "The arguments to pass to the command. (Optional)");
+        lines[3] = new KeyValuePair<string, string>("TARGET", "The file to write the output too.");
+
+        return lines;
+    }
+
+    public override bool IsValid(ArgumentsModule args)
+    {
+        if (!args.HasOverrideArgument("COMMAND"))
+        {
+            Log.WriteLine("A COMMAND must be defined (---COMMAND=echo");
+            return false;
+        }
+
+        if (!args.HasOverrideArgument("TARGET"))
+        {
+            Log.WriteLine("A COMMAND must be defined (---TARGET=c:\\output.txt");
+            return false;
+        }
+
+        return base.IsValid(args);
+    }
+
+    public override void ParseArguments(ArgumentsModule args)
+    {
+        Command = args.GetOverrideArgument("COMMAND");
+        Target = args.GetOverrideArgument("TARGET");
+
+        if (args.HasOverrideArgument("WORKING-DIRECTORY"))
+        {
+            WorkingDirectory = args.GetOverrideArgument("WORKING-DIRECTORY");
+        }
+
+        if (args.HasOverrideArgument("ARGUMENTS"))
+        {
+            Arguments = args.GetOverrideArgument("ARGUMENTS");
+        }
+    }
+}
---
+++ Applications/K9.OS.OutputToFile/Program.cs
@@ -0,0 +1,47 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+using System;
+using System.Net;
+using K9.Core;
+using K9.Core.Utils;
+
+namespace K9.OS.OutputToFile;
+
+internal static class Program
+{
+    static void Main()
+    {
+       using ConsoleApplication framework = new(
+            new ConsoleApplicationSettings()
+            {
+                // ReSharper disable once StringLiteralTypo
+                DefaultLogCategory = "OUTPUT",
+                LogOutputs = [new Core.LogOutputs.ConsoleLogOutput()]
+            }, new OutputToFileProvider());
+
+        try
+        {
+            OutputToFileProvider provider = (OutputToFileProvider)framework.ProgramProvider;
+
+            ProcessLogCapture outputCapture = new();
+
+            ProcessUtil.Execute(provider.Command, provider.WorkingDirectory, provider.Arguments,
+                null, outputCapture.GetAction());
+
+            if (outputCapture.HasContent())
+            {
+                System.IO.File.WriteAllLines(provider.Target, outputCapture.GetLines());
+                Log.WriteLine($"Content wrote to {provider.Target}.");
+            }
+            else
+            {
+                Log.WriteLine($"No content was written to {provider.Target}.", ILogOutput.LogType.Error);
+            }
+        }
+        catch (Exception ex)
+        {
+            framework.ExceptionHandler(ex);
+        }
+    }
+}
