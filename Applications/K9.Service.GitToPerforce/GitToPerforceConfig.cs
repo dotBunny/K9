da2aa03b165773d6f2975ae9de62a30ac885973b
Matthew Davey <reapazor@gmail.com>
2026-05-04 22:48:28 -0400

    Update with proper pathing

--- Applications/K9.Service.GitToPerforce/GitToPerforceConfig.cs
+++ Applications/K9.Service.GitToPerforce/GitToPerforceConfig.cs
@@ -20,12 +20,12 @@ public class GitToPerforceConfig
     public string? PerforceUsername { get; set; }
     public string? PerforcePassword { get; set; }
     public string PerforceClientName { get; set; } = "K9_GitToPerforce_Sync";
-    public string PerforceWorkspaceStreamName { get; set; } = "//UE5/NEXUS-SYNC";
+    public string PerforceWorkspaceStreamName { get; set; } = "//NEXUS/NEXUS-Sync";
     public string PerforceCommitMessageTemplate { get; set; } = "#K9 Updated Git repository at $GitPath to $GitHash";
 
     public string GitRepositoryUrl { get; set; } = "https://github.com/dotBunny/NEXUS.git";
     public string GitBranch { get; set; } = "main";
-    public string GitRepositoryRelativeRoot { get; set; } = "Projects/DETHOL/Plugins/NEXUS";
+    public string GitRepositoryRelativeRoot { get; set; } = "NEXUS";
     // ReSharper restore PropertyCanBeMadeInitOnly.Global
 
     public static GitToPerforceConfig Get(string? jsonPath = null)
